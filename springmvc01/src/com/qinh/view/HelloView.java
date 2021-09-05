80eba6eed79b5420d81b9d70297c269d0a0bdf53
Qh92 <771600967@qq.com>
2021-09-06 00:01:53 +0800

    feat:添加新的思维导图

--- springmvc01/src/com/qinh/view/HelloView.java
+++ springmvc01/src/com/qinh/view/HelloView.java
@@ -9,6 +9,27 @@ import java.time.LocalDateTime;
 import java.util.Map;
 
 /**
+ * 自定义视图，必须放进spring容器
+ *
+ * BeanNameViewResolver#resolveViewName(String viewName, Locale locale)
+ * public View resolveViewName(String viewName, Locale locale) throws BeansException {
+ * 		ApplicationContext context = obtainApplicationContext();
+ * 		if (!context.containsBean(viewName)) {
+ * 			// Allow for ViewResolver chaining...
+ * 			return null;
+ *                }
+ * 		if (!context.isTypeMatch(viewName, View.class)) {
+ * 			if (logger.isDebugEnabled()) {
+ * 				logger.debug("Found bean named '" + viewName + "' but it does not implement View");
+ *            }
+ * 			// Since we're looking into the general ApplicationContext here,
+ * 			// let's accept this as a non-match and allow for chaining as well...
+ * 			return null;
+ *        }
+ * 		return context.getBean(viewName, View.class);*
+ * }
+ *
+ *
  * @author Qh
  * @version 1.0
  * @date 2021-04-25-23:32
